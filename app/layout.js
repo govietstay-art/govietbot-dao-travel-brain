6d2a3cf2f450589d36edd1e31ca834f1ae5ca68d
govietstay-art <govietstay@gmail.com>
2026-06-08 09:56:38 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "Ask Đào | GoVietStay",
+  description: "GoVietStay Local Travel Assistant"
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
